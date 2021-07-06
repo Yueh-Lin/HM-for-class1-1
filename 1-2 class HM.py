daf7cb2052811aba5d34e8c804c63d58eb515b92
Yueh-Lin <86942074+Yueh-Lin@users.noreply.github.com>
2021-07-06 14:19:31 +0800

    Create 1-2 class HM.py

---
+++ 1-2 class HM.py
@@ -0,0 +1,11 @@
+num=input ('num score?')
+eng=input ('eng score')
+
+eng＝int(eng)
+num=int(num)
+
+if eng > 90 and num > 90:
+    print("have gift")
+    
+if eng < 90 and num < 90:
+    print("Punishment")
