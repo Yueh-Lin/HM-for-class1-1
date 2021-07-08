da627676f22a14bcd9e6f2e81a4b543eb272fc28
Yueh-Lin <86942074+Yueh-Lin@users.noreply.github.com>
2021-07-08 16:27:27 +0800

    Add files via upload

---
+++ Class and HM 1-4 class.py
@@ -0,0 +1,47 @@
+1.) for x in [1,2,3]:
+    print(x)
+
+2.)for x in 'ABC':
+    print(x)
+
+3.)for i in [0,1,2,3,4,5]:
+    print(i)
+    
+for i in range (6):
+    print('i:',i) 
+    
+4.)for x in [0,1,2]:
+    print(x)
+
+for x in range (0,3):
+    print(x)
+
+5.)for i in range(4, 8, 5):
+    print('x:',i)
+    
+for i in [4]:
+    print('x:',i)
+
+6.)for i in range (0, 50, 3):
+    print('x:',i) 
+    
+for i in [x: 0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48]
+    print('x:',i)
+    
+7.)for x in range (1,11):
+    print('*'*x)
+
+HM:
+for x in range (11,0,-1):
+    print('*'*x) 
+    
+
+自創：
+for x in range (1,11):
+    print('*'*x)
+
+for x in range (11,0,-1):
+    print('*'*x) 
+    
+    
+
