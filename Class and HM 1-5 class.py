8da242e9358b0a16aede72e7404fe2074c3eba2a
Yueh-Lin <86942074+Yueh-Lin@users.noreply.github.com>
2021-07-11 13:27:06 +0800

    Add files via upload
    finished!!!

---
+++ Class and HM 1-5 class.py
@@ -0,0 +1,96 @@
+part 1.) review yesterday class (not showing, beacuse it too long.)
+
+part 2.) reviewing part two using for / while loop to make an 倒星星塔:
+
+for x in range (11,0,-1):
+    print('*'*x) 
+    
+    x=10
+while x > 0:
+    print('*'*x)
+    x=x-1   
+    
+part 3.)  list (starting class)
+   
+方法1
+score=["90,Joesph,89,Curry,79,Annie,36,Daniel"]
+print(score)
+
+方法2
+score=list()
+score.append(98)
+print('person 1',score)
+score.append(100)
+print('person 2',score)
+score.append(60)
+print('person 3',score) 
+
+方法3
+score=list
+
+score=[100,95,98]
+
+for i in score:
+    print(i)
+
+i=0
+while i < 4:
+    print("i",i,score[i])
+    i=i+1
+
+others about this?
+    
+score=[100,90,98,70,50]
+
+score.remove(100)
+print(score)
+
+score=[100,90,98,70,50]
+
+score.insert(0,1000)
+
+print(score)
+
+先練習一下和回家作業相似的東西吧！
+
+things=list
+things=["a","the"," an", "article"]
+
+things.insert(3,3)
+things.remove("article")
+print(things) 
+
+功課
+score=list
+print("their score is:")
+score=["99'Leona'100'Olivia'"]
+
+    
+Olivia=99
+
+Leona=100
+
+for i in score:
+    print(i)
+    
+if 3 > 2 and 3 > 2:
+    print("Leona's score is better")
+    print("相差") 
+    print(Leona-Olivia)
+
+if 2 > 3 and 2 > 3:
+    print("Olivia's score is better")
+    print("相差")
+    print(Olivia-Leona)
+
+if 2 > 3 and 2 > 3:
+    print(Olivia-Leona)
+
+if 3 == 2:
+    print("thery're both good")
+    
+print("他們的平均數是：")
+print(Olivia+Leona/2) 
+
+
+
