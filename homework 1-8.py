3c1b824a47cd8de64306af01fe0360025240af11
Yueh-Lin <86942074+Yueh-Lin@users.noreply.github.com>
2021-07-14 16:27:45 +0800

    Add files via upload

---
+++ homework 1-8.py
@@ -0,0 +1,36 @@
+a=int(input('有幾個蘋果？'))
+b=int(input('一個蘋果幾塊錢？'))
+
+c=int(input('廠商給幾個蘋果？'))
+
+print("現在有？")
+x=print(a+c)
+
+
+if c > a:
+    print("error")
+    
+
+d=int(input('你今天要賣幾個？')) 
+if x == d:
+    print("that is all of your apple, next step.")
+
+if c == b:
+    print("所有的蘋果今天即將賣出！")
+    
+z=1
+e=d*b 
+if z ==1:
+    print("你今天賺了：")
+    print(e) 
+    
+
+f=int(input('離開系統？'))
+
+yes=1
+
+if 1 == yes:
+    print("bye bye") 
+    
+else:
+    print("play again!!!")
